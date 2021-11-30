6d860558271ca3af84ceaf7ff8a41221cf43f5f6
vojta1429 <95287318+vojta1429@users.noreply.github.com>
2021-11-30 13:08:45 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Kanonýři U13 volnočas</title>
+  <link rel="icon" type="image/x-icon" href="https://www.kanonyrikladno.cz/img/logo_default.png">
+<body>
+<h1 align="center">Kanonýři Kladno U13</h1>
+  <body background="pozadi.gif" bgcolor="3241CZ">
+    <h1 align="center">
+<hl>Dobrý den vítejte na stránkách týmu Kanonýři Kladno U13-volnočas</hl>
+<p>Zde se dozvíte pouze pár informací o nás zbytek se dočtete na kanonýrských stránkách a z informací od trenérů</p>
+
+
+<img src="https://www.kanonyrikladno.cz/img/logo_default.png" alt="Logo" style="align-items: center;">
+
+<p><a href="https://www.kanonyrikladno.cz/tym/129-u13-mladsi-zaci-volnocas-09-a-10.html">Tréninky:</a></p>
+<p>Kontakty na trenéry:</p>
+<p>František Novohradský (hlavní trenér)</p>
+<p>Matěj Zíka (asistent trenéra)</p>
+<p>Vojtěch Vaňous (asistent trenéra)</p>
+<table border="1px border bgcolor black" align="center">
+  <tr style="text-anchor: ">
+    <th>Pondělí</th>
+    <td>4.ZŠ Kladno</td>
+    <td>17:00</td>
+  </tr>
+  <tr>
+    <th>Úterý</th>
+    <td>12.ZŠ Kladno</td>
+    <td>16:30</td>
+  </tr>
+  <tr>
+    <th>Středa</th>
+    <td>12.ZŠ Kladno</td>
+    <td>16:00</td>
+  </tr>
+</table>
+<a href="https://www.kanonyrikladno.cz/tym/129-u13-mladsi-zaci-volnocas-09-a-10/tabulka.html" style="align-content: left">
+
+
+</body>
+</html>
